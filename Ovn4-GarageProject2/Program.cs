46851c33ab0280f9b6f6c8706b57ae49be042de6
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-19 21:26:19 +0200

    fix(handler): update vehicle parking spots and adjust seed data

--- Ovn4-GarageProject2/Program.cs
+++ Ovn4-GarageProject2/Program.cs
@@ -7,12 +7,13 @@ var ui = new ConsoleUi(handler);
 var manager = new Manager(ui, handler);
 handler.SetGarage(manager.ActiveGarage);
 // seed garage with some vehicles
-handler.ParkAtSpot(20, new Ovn4_GarageProject2.Domain.Car
+// IDs are r*16+c for the 16-column blueprint.
+handler.ParkAtSpot(21, new Ovn4_GarageProject2.Domain.Car
     { RegNumber = "DEF345", Colour = "Blue", WheelCount = "4", FuelType = "EV" });
-handler.ParkAtSpot(22, new Ovn4_GarageProject2.Domain.Motorcycle
+handler.ParkAtSpot(23, new Ovn4_GarageProject2.Domain.Motorcycle
     { RegNumber = "DEF456", Colour = "Black", WheelCount = "2", CylinderVolume = "650" });
-handler.ParkAtSpot(84, new Ovn4_GarageProject2.Domain.Car
+handler.ParkAtSpot(89, new Ovn4_GarageProject2.Domain.Car
     { RegNumber = "ABC123", Colour = "Red", WheelCount = "4", FuelType = "Gasoline" });
-handler.ParkAtSpot(28, new Ovn4_GarageProject2.Domain.Bus()
-    { RegNumber = "ABC123", Colour = "Red", WheelCount = "4", NumberOfSeats = "12"});
+handler.ParkAtSpot(29, new Ovn4_GarageProject2.Domain.Bus()
+    { RegNumber = "BUS001", Colour = "Yellow", WheelCount = "6", NumberOfSeats = "12"});
 manager.Run();
